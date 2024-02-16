e8a56ee96472b61c44fd94ff10da712f64f370b4
Sofia Leon <sofialeon@google.com>
2024-02-16 11:24:44 -0800

    retry

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { logger } from 'firebase-functions/v1';
 import { onRequest } from 'firebase-functions/v2/https';
 import { initializeApp } from 'firebase-admin/app';
 import { CloudTasksClient } from '@google-cloud/tasks'; 
-
+import {SecretManagerServiceClient} from '@google-cloud/secret-manager'
 initializeApp();
 
 
@@ -10,9 +10,10 @@ const testFunction = onRequest(async (request, response) => {
 
   logger.info('Hello testFunction!');
 
-  await cloudTask('your project');
+  const queues = await cloudTask('projects/coleleah-sofialeon/locations/us-central1');
 
-  response.send(`Firebase function run OK with cloudTask client`);
+  const secrets = await secretManager('projects/coleleah-sofialeon')
+  response.send(`Firebase function run OK with cloudTask client: ${JSON.stringify(queues)}, ${JSON.stringify(secrets)}`);
 });
 
 exports.testFunction = testFunction;
@@ -20,13 +21,15 @@ exports.testFunction = testFunction;
 
 async function cloudTask(parent: string) {
     const tasksClient = new CloudTasksClient(); 
-    async function callListQueues() {
-    // just checking tasksClient is not empty
-      await tasksClient.close();
-    }
-  
-    callListQueues();
+
+    return await tasksClient.listQueues({parent});
     // [END cloudtasks_v2_generated_CloudTasks_ListQueues_async]
+  }
+
+  async function secretManager(parent: string) {
+    const secretmanagerclient = new SecretManagerServiceClient();
+    return await secretmanagerclient.listSecrets({parent})
+
   }
   
 //   process.on('unhandledRejection', err => {
